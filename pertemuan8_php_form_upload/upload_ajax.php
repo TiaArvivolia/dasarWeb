e97fb110136219a00b447202057769a8be83d53f
TiaArvivolia <arvivoliatia@gmail.com>
2024-04-04 09:44:15 +0700

    Praktikum 4 : Menghias Upload File

--- pertemuan8_php_form_upload/upload_ajax.php
+++ pertemuan8_php_form_upload/upload_ajax.php
@@ -5,10 +5,10 @@ if(isset($_FILES['file'])) {
     $file_size = $_FILES['file']['size'];
     $file_tmp = $_FILES['file']['tmp_name'];
     $file_type = $_FILES['file']['type'];
-    @$file_ext = strtolower("" . end(explode('.', $_FILES['file']['name'])) . "");
+    $file_ext = strtolower(pathinfo($file_name, PATHINFO_EXTENSION));
     $extensions = array("pdf", "doc", "docx", "txt");
 
-    if(in_array($file_ext, $extensions) === false) {
+    if(!in_array($file_ext, $extensions)) {
         $errors[] = "Ekstensi file yang diizinkan adalah PDF, DOC, DOCX, atau TXT.";
     }
 
@@ -16,10 +16,11 @@ if(isset($_FILES['file'])) {
         $errors[] = 'Ukuran file tidak boleh lebih dari 2 MB';
     }
     
-    if(empty($errors) == true) {
-        move_uploaded_file($file_tmp, "documents/" .$file_name);
+    if(empty($errors)) {
+        move_uploaded_file($file_tmp, "documents/" . $file_name);
         echo "File berhasil diunggah.";
     } else {
         echo implode(" ", $errors);
     }
 }
+?>
