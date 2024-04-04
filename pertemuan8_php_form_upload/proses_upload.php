2de6bf486178357c423baeaad77584ec948a288e
TiaArvivolia <arvivoliatia@gmail.com>
2024-04-04 08:08:00 +0700

    Praktikum 2 : Multiupload File

--- pertemuan8_php_form_upload/proses_upload.php
+++ pertemuan8_php_form_upload/proses_upload.php
@@ -7,16 +7,16 @@ if (!file_exists($targetDirectory)) {
     mkdir($targetDirectory, 0777, true);
 }
 
-if($_FILES['files']['name'][0]) {
+if ($_FILES['files']['name'][0]) {
     $totalFiles = count($_FILES['files']['name']);
 
     // Loop melalui semua file yang diunggah
-    for ($i=0; $i<$totalFiles; $i++) {
+    for ($i = 0; $i < $totalFiles; $i++) {
         $fileName = $_FILES['files']['name'][$i];
-        $targetFile = $targetDirectory.$fileName;
+        $targetFile = $targetDirectory . $fileName;
 
         // Pindahkan file yang diunggah ke direktori penyimpanan
-        if(move_uploaded_file($_FILES['files']['tmp_name'][$i], $targetFile)) {
+        if (move_uploaded_file($_FILES['files']['tmp_name'][$i], $targetFile)) {
             echo "File $fileName berhasil diunggah. <br>";
         } else {
             echo "Gagal mengunggah file $fileName . <br>";
@@ -25,5 +25,3 @@ if($_FILES['files']['name'][0]) {
 } else {
     echo "Tidak ada file yang diunggah.";
 }
-
-    
