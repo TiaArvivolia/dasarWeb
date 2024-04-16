248d7f2763e045d001939e5191da3f0cbabfa797
TiaArvivolia <arvivoliatia@gmail.com>
2024-04-16 21:32:35 +0700

    Praktikum 2, 3, 4 Bagian Proses

--- pertemuan10_php_crud/crud/proses.php
+++ pertemuan10_php_crud/crud/proses.php
@@ -31,5 +31,23 @@ if ($aksi == 'tambah') {
         echo "id tidak valid";
     }
 }
+else if ($aksi == 'hapus'){
+    if(isset($_GET['id'])){
+        $id = $_GET['id'];
+
+        $query = "DELETE FROM anggota WHERE id = $id";
+        if(mysqli_query($koneksi,$query)){
+            header("Location: index.php");
+            exit();
+        }
+        else {
+            echo "Gagal menghapus data: " . mysqli_error($koneksi);
+        }
+    }
+    else {
+        echo "id tidak valid";
+    }
+}
 
 mysqli_close($koneksi);
+?>
